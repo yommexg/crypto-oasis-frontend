d095d3a8b4eb18b44b8e5e504caf4eb4a98af4bf
yommexg <hydrokidb@gmail.com>
2025-06-12 23:50:05 +0100

    Update app content

--- src/AppContent.tsx
+++ src/AppContent.tsx
@@ -15,9 +15,9 @@ function AppContent() {
   const location = useLocation();
 
   const { isAuthLoading } = useAuth();
-  const { getUser, isUserLoading } = useUser();
+  const { getUser, isUserLoading, user } = useUser();
 
-  const user = true;
+  // const user = true;
 
   const [acceptedTerms, setAcceptedTerms] = useState(false);
   const [acceptedCookies, setAcceptedCookies] = useState(false);
