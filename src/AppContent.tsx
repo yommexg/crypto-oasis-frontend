f8ef23382fb915fd3bf2ed87c757f98b8e73fd46
yommexg <hydrokidb@gmail.com>
2025-06-12 16:49:43 +0100

    Update walletfunctions

--- src/AppContent.tsx
+++ src/AppContent.tsx
@@ -15,9 +15,9 @@ function AppContent() {
   const location = useLocation();
 
   const { isAuthLoading } = useAuth();
-  const { getUser } = useUser();
+  const { getUser, user } = useUser();
 
-  const user = true;
+  // const user = true;
 
   const [acceptedTerms, setAcceptedTerms] = useState(false);
   const [acceptedCookies, setAcceptedCookies] = useState(false);
