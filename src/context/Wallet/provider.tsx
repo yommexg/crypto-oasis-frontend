c8f19a2aafd5af790f234076376f62da0c3e1c43
yommexg <hydrokidb@gmail.com>
2025-06-27 16:04:15 +0100

    local storage to stire  address

--- src/context/Wallet/provider.tsx
+++ src/context/Wallet/provider.tsx
@@ -1,18 +1,25 @@
 import { useEffect, useState, type ReactNode } from "react";
 import { WalletContext } from "./context";
-import { useLocation } from "react-router-dom";
 
 export const WalletProvider = ({ children }: { children: ReactNode }) => {
   const [walletLoading, setWalletLoading] = useState(false);
-  const [address, setAddress] = useState<`0x${string}` | undefined>(undefined);
+
+  const [address, setAddress] = useState<`0x${string}` | undefined>(() => {
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("walletAddress") as `0x${string}` | undefined;
+    }
+    return undefined;
+  });
+
   const [activeWallet, setActiveWallet] = useState<string | null>(null);
 
-  const location = useLocation();
-
   useEffect(() => {
-    setAddress(address);
-    setActiveWallet(activeWallet);
-  }, [address, activeWallet, location]);
+    if (address) {
+      localStorage.setItem("walletAddress", address);
+    } else {
+      localStorage.removeItem("walletAddress");
+    }
+  }, [address]);
 
   return (
     <WalletContext.Provider
